bcdc23689e48d73f0df7351552410926cf86faa3
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-10-29 21:38:48 -0500

    made a bunch of different things work

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,10 +3,15 @@ import 'bootstrap';
 import '../styles/main.scss';
 
 import apiKeys from './helpers/apiKeys.json';
+import auth from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/MyNavbar/myNavbar';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  console.error('hi', apiKeys.firebaseKeys);
+  auth.loginButton();
+  authData.checkLoginStatus();
+  myNavbar.logoutEvent();
 };
 
 init();
